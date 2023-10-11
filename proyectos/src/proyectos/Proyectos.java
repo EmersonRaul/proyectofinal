79fed7fbdd610e1abd88c2dd7cd375dd3d5aa660
EmersonRaul <marlonraulhernandez199@gmail.com>
2023-10-11 06:41:01 -0600

    Proyecto

---
+++ proyectos/src/proyectos/Proyectos.java
@@ -0,0 +1,17 @@
+
+package proyectos;
+
+public class Proyectos {
+
+    public static void main(String[] args) {
+        Principal inicio = new Principal();
+
+        inicio.setBounds(510, 150, 480, 460);
+
+        inicio.setTitle("Inicio de Sesion");
+
+        inicio.iniciarComponentes();
+        
+    }
+    
+}
